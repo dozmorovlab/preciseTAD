b5f2e1857df47c1c45a1947448622c1c2f06fcc3
stilianoudakis <stilianoudasc@mymail.vcu.edu>
2020-11-24 15:00:41 -0500

    added column names

--- R/juicer_func.R
+++ R/juicer_func.R
@@ -74,29 +74,30 @@
 #' }
 juicer_func <- function(grdat) {
     # n <- length(unique(as.character(seqnames(grdat))))
-
+    
     chrs <- unique(as.character(seqnames(grdat)))
-
+    
     mat_list <- list()
-
+    
     for (i in seq_len(length(chrs))) {
         grdat_chr <- grdat[which(as.character(seqnames(grdat)) == chrs[i])]
-
+        
         if (length(grdat_chr) > 1) {
             mymat <- data.frame(matrix(nrow = (length(grdat_chr) - 1), ncol = 6))
-            mymat[, 1] <- mymat[, 4] <- chrs[i]
+            mymat[, 1] <- mymat[, 4] <- gsub("chr", "", chrs[i])
             for (j in seq_len(length(grdat_chr) - 1)) {
                 mymat[j, 2] <- mymat[j, 5] <- start(grdat_chr)[j]
                 mymat[j, 3] <- mymat[j, 6] <- start(grdat_chr)[j + 1]
             }
-
+            
             mat_list[[i]] <- mymat
-
+            
         }
-
+        
     }
-
+    
     mat_list <- do.call("rbind.data.frame", mat_list)
+    names(mat_list) <- c("chr1", "x1", "x2", "chr2", "y1", "y2")
     return(mat_list)
-
+    
 }
